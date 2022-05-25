6dc7fcca7b63aeef27283229b0e1ec90e5fa0426
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-05-25 21:59:36 +0430

    add field to room & message table

--- create.sql
+++ create.sql
@@ -22,7 +22,7 @@ CREATE TABLE public."contacts"
 
 CREATE TABLE public."room" 
 (
-	id serial NOT NULL,
+	id serial NOT NULL UNIQUE,
 	creator bigint not null,
 	info character varying(512),
 	profile_photo character varying(128),
@@ -30,5 +30,23 @@ CREATE TABLE public."room"
 	create_date date,
 	username character varying(56) not null UNIQUE,
 	CONSTRAINT proper_username CHECK (username ~* '^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'),
+	PRIMARY KEY (id),
 	foreign key (creator) references "user" (id)
+);
+
+CREATE TABLE public."message" 
+(
+	parent_id bigint UNIQUE,
+	id serial NOT NULL,
+	sender_id bigint not null,
+	room_receiver_id bigint not null,
+	user_receiver_id bigint not null,
+	text character varying(1024),
+	sended_time date,
+	PRIMARY KEY (id),
+	foreign key (sender_id) references "user" (id),
+	foreign key (user_receiver_id) references "user" (id),
+	foreign key (room_receiver_id) references "room" (id),
+	foreign key (parent_id) references "message" (id),
+	CONSTRAINT check_receiver check (room_receiver_id is not null or user_receiver_id is not null)
 );
