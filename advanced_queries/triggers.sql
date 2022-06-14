a8a9026cbce9bc9e5ff76cbdfcdafb376bf8ae0e
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-06-14 23:08:08 +0430

    trigger convert_to_uppercase_name_field_for_user

--- advanced_queries/triggers.sql
+++ advanced_queries/triggers.sql
@@ -19,3 +19,21 @@ DROP TRIGGER trigger_room_username ON "room" CASCADE;
 INSERT INTO "room" (creator, type, username, create_date)
 VALUES (1, 'channel', 'var', '2018-05-06');
 
+-- تبدیل فیلد نیم به حروف بزرگ برای یوزر با استفاده از تریگر
+CREATE OR REPLACE FUNCTION convert_to_uppercase_name_field_for_user() RETURNS TRIGGER AS $$
+BEGIN
+	NEW.name = UPPER(NEW.name);
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER  upgrade_name_field_for_user
+BEFORE INSERT ON "user"
+FOR EACH ROW
+EXECUTE PROCEDURE convert_to_uppercase_name_field_for_user();
+
+INSERT INTO "user" (name, username, phone_number)
+VALUES ('mojtaba', 'mojtaba', '09111234561');
+select * from "user";
+
+
