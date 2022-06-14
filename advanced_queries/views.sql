ef7605a3e1543caa1b3b29f47746cacdc7585743
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-06-14 22:45:31 +0430

    add MATERIALIZED VIEW message_without_reply_in_group_materilized_view

--- advanced_queries/views.sql
+++ advanced_queries/views.sql
@@ -6,4 +6,11 @@ select * from "message" where room_receiver_id = 1 and sender_id in (
 
 SELECT * FROM messages_from_my_contacts_in_specific_room;
 
--- 
+-- پیامهای یک گروه با آیدی خاص که روی آنها ریپلای نشده است در قالب ویو
+CREATE MATERIALIZED VIEW message_without_reply_in_group_materilized_view AS
+select "message".id, text from "message" inner join "room"
+on "message".room_receiver_id = "room".id
+where type='group' and "room".id = 1 and parent_id is null ;
+
+REFRESH MATERIALIZED VIEW message_without_reply_in_group_materilized_view;
+SELECT * FROM message_without_reply_in_group_materilized_view;
