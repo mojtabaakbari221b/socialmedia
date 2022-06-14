a889ab1402dc1d3e40d32e5c0185de73d39f6347
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-06-15 01:17:01 +0430

    add sp adding new user to room

--- advanced_queries/stored_procedures.sql
+++ advanced_queries/stored_procedures.sql
@@ -16,3 +16,21 @@ $$;
 
 CALL add_new_user('mojtaba','sample','09119111245');
 select * from "user";
+
+-- متد برای اضافه کردن یک یوزر به یک روم
+CREATE OR REPLACE PROCEDURE add_new_user_to_room(
+	new_user_id integer,
+	destination_room_id integer
+)
+LANGUAGE plpgsql    
+AS 
+$$
+BEGIN
+	INSERT INTO "room_users" (user_id, room_id)
+	VALUES (new_user_id, destination_room_id);
+COMMIT;
+END;
+$$;
+
+CALL add_new_user_to_room(1 , 2);
+select * from "room_users";
